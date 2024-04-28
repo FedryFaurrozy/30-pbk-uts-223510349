a366d0e045f979b231dc5fd9f74ce7443c87f120
Fedry Faurrozy <istanap303@gmail.com>
2024-04-28 13:13:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1>Aplikasi To-Do List</h1>
+    <todo-list />
+  </div>
+</template>
+
+<script>
+import TodoList from "./TodoList.vue";
+
+export default {
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  text-align: center;
+  font-family: sans-serif;
+}
+</style>
